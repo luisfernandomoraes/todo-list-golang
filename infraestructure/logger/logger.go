74cfcc5842b2a407906929257f39d73740c756d4
Luís Fernando Moraes <luisfernando.pereira.moraes@gmail.com>
2022-09-25 15:33:52 -0300

    Implementing zerolog

---
+++ infraestructure/logger/logger.go
@@ -0,0 +1,27 @@
+package logs
+
+import (
+	"os"
+	"strings"
+
+	envs "github.com/luisfernandomoraes/todo-list-golang/infraestructure/envs"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+var logger zerolog.Logger
+
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	value := envs.GetEnvironmentVariableByKey("ENVIRONMENT")
+	if strings.ToUpper(value) == "DEVELOPMENT" {
+		logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	} else {
+		logger = zerolog.New(os.Stderr).With().Timestamp().Caller().Logger()
+	}
+}
+
+func GetLogger() zerolog.Logger {
+	return logger
+}
